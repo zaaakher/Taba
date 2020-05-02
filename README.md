a7109a74f7da6835c0ca4aedefe225a3625afb02
Ryan Masri <RayanMasri11>
2020-05-03 02:35:26 +0300

    added better renaming

---
+++ README.md
@@ -0,0 +1,32 @@
+Things to add:
+
+- Add the favicon of each link [DONE]
+- Add x next to each link to delete the link [DONE]
+- Each session has a "delete" option to delete the session [DONE]
+- Each session has a "restore" option to restore the links back to tabs [DONE]
+- When a link is clicked, it is opened in a new tab and removed from the list. [DONE]
+- When a link is clicked, the focus remains on the Taba tab. [DONE]
+- Reduce amount of reloads
+- Merging sessions
+- If taba is active save all tabs and refresh the active page
+- Drag and drop from session to session
+- Drag and drop from tab to tab within session
+
+
+Things to fix:
+
+- Pinned tabs are still included in the links array [DONE]
+- The Taba tab itself is also included in the links [DONE]
+- Pinned tabs prevent collection
+- Some HTML is present in other sites after closing the holder
+- When refreshing(programmatically) retain scroll value
+
+Things to improve code reading:
+
+- Use getSelected instead of query({active: true, currentWindow: true})
+
+Things to do for CSS:
+- And have the same height as the amount of lines of the input
+- Add a font
+- Separate the buttons "Restore All", "Delete All", "Rename", etc.
+- Adjust input width for .session-name to match the input
---
+++ background.js
@@ -0,0 +1,69 @@
+Number.prototype.pad = function (size) {
+	var s = String(this);
+	while (s.length < (size || 2)) {
+		s = "0" + s;
+	}
+	return s;
+}
+
+function get_time() {
+	//return moment().format("MM:DD:YYYY, hh:mm:ss A");
+	let today = new Date();
+	let date_string = `${(today.getMonth() + 1).pad()}/${today.getDate().pad()}/${today.getFullYear().pad(4)}, ${today.getHours() % 12}:${today.getMinutes().pad()}:${today.getSeconds().pad()} ${today.getHours() > 12 ? "PM" : "AM"}`
+
+	return date_string
+}
+
+let newtab_url = "chrome://newtab/"
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+	//console.log("clicked")
+	let query_info = {
+		"currentWindow": true
+	}
+	let tab_info = {
+		"index": 0,
+		"url": "index.html"
+	}
+	chrome.tabs.create(tab_info)
+	chrome.tabs.query({
+		active: true,
+		currentWindow: true
+	}, function (selected) {
+		let main_url = (selected[0].url || selected[0].pendingUrl);
+		chrome.tabs.query(query_info, (tabs) => {
+			let session = []
+			for (let i = 1; i < tabs.length; i++) {
+				if (!tabs[i].pinned) {
+					try {
+						if (!(tabs[i].url == main_url || tabs[i].url == newtab_url)) {
+							session.push(tabs[i])
+						}
+						chrome.tabs.remove(tabs[i].id)
+						console.log(`removed ${tabs[i].url} with id ${tabs[i].id}`)
+					} catch {
+						console.log("could not remove " + tabs[i].url)
+					}
+				}
+			}
+
+			session = [session, get_time(), '', `${session.length} tabs`]
+
+			chrome.storage.local.get({
+				'key': []
+			}, function (result) {
+				let sessions = result.key
+				if (!Array.isArray(sessions)) {
+					sessions = []
+				}
+
+				sessions.push(session)
+				//console.log(session)
+				chrome.storage.local.set({
+					'key': sessions
+				});
+			});
+
+		})
+	});
+});
